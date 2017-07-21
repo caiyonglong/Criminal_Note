dfe50d5272ca044d6b49d6bda49c17180173ca25
unknown <D22434@SZCKT.COM>
2017-07-21 16:40:21 +0800

    Initial commit

---
+++ app/src/main/java/com/ck_telecom/d22434/criminal_note/Crime.java
@@ -0,0 +1,44 @@
+package com.ck_telecom.d22434.criminal_note;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by D22434 on 2017/7/21.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setTitle(String title) {
+        this.mTitle = title;
+    }
+
+    public void setSolved(boolean solved) {
+        this.mSolved = solved;
+    }
+}
